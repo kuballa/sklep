4186d96f8d6c3b986d73411c2cfb869f1f2a2032
Jakub <jziomek669@gmail.com>
2023-01-16 17:48:53 +0100

    search not completed

--- frontend/src/app/components/pages/home/home.component.ts
+++ frontend/src/app/components/pages/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 import { FoodService } from 'src/app/services/food.service';
 import { Food } from 'src/app/shared/models/Food';
 
@@ -7,13 +8,19 @@ import { Food } from 'src/app/shared/models/Food';
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
-export class HomeComponent implements OnInit{
-  foods: Food[] = []
-  constructor(private foodService: FoodService) {
-    this.foods = foodService.getAll();
-   }
+export class HomeComponent implements OnInit {
+
+  foods:Food[] = [];
+  constructor(private foodService:FoodService, activatedRoute:ActivatedRoute) {
+    activatedRoute.params.subscribe((params) => {
+      if(params.searchTerm)
+      this.foods = this.foodService.getAllFoodsBySearchTerm(params.searchTerm);
+      else
+      this.foods = foodService.getAll();
+    })
+
+  }
 
   ngOnInit(): void {
-    
   }
 }
--- frontend/src/app/services/food.service.ts
+++ frontend/src/app/services/food.service.ts
@@ -10,10 +10,11 @@ export class FoodService {
   constructor() { }
 
   getAll(): Food[]{
-    return sample_foods
+    return sample_foods;
   }
 
-  getAllFoodsBySearchTerm(searchTerm: string) {
-    return this.getAll().filter(food => food.name.toLocaleLowerCase().includes(searchTerm.toLocaleLowerCase()))
+  getAllFoodsBySearchTerm(searchTerm:string){
+    return this.getAll().filter(food => food.name.toLowerCase().includes(searchTerm.toLowerCase()))
   }
+
 }
